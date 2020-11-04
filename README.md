415a50485a83da05c48da5b2d9a466bde55eee2f
Harsh Sharma <sharmaharsh2308@gmail.com>
2020-11-04 10:00:43 -0500

    modified test.launch to work with kitti;added open3d to condaenv;pcd subscriber added

--- README.md
+++ README.md
@@ -5,21 +5,27 @@ Tracking only using cameras with a prior lidar map. We pose the problem on a fac
 
 ### ROS Melodic Ubuntu 18.04
 
+
+### Conda environment
+```
+conda env create -f conda_env.yml --name slam
+```
+
 ### Create ROS-workspace
 ```
 mkdir -p ros_ws/src
 cd ros_ws/src
 git clone https://github.com/Harsharma2308/camTracking_GTSAM.git
 cd ..
-catkin_make
-
+catkin_make --cmake-args -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=~/anaconda3/envs/slam/bin/python3  -DPYTHON_INCLUDE_DIR=~/anaconda3/envs/slam/include/python3.7m    -DPYTHON_LIBRARY=~/anaconda3/envs/slam/lib/libpython3.7m.so
 ```
 
 
 ### Usage (Make sure to match topics)
 ```
-rosbag play --loop MH_01_easy.bag
+rosbag play --loop kitti.bag
 roslaunch cam_tracking run.launch
+python run.py
 ```
 
 
--- cam_tracking/src/run.py
+++ cam_tracking/src/run.py
@@ -1,10 +1,16 @@
+import sys
+paths = ['/home/stars/Documents/sof/slam/project/ros_ws/devel/lib/python3/dist-packages', '/home/stars/anaconda3/envs/slam/lib/python37.zip', '/home/stars/anaconda3/envs/slam/lib/python3.7', '/home/stars/anaconda3/envs/slam/lib/python3.7/lib-dynload', '/home/stars/anaconda3/envs/slam/lib/python3.7/site-packages']
+sys.path.extend(paths);sys.path.reverse()
 import rospy
 import math
 import tf
 import geometry_msgs.msg
 import tf2_ros
-import ipdb; ipdb.set_trace()
-
+import pcl_ros
+import sensor_msgs.msg
+import numpy as np
+import open3d as o3d
+import ros_numpy
 
 class TrackerNode:
     def __init__(self,name):
@@ -14,10 +20,39 @@ class TrackerNode:
         self._refined_pose=None
         self._map_frameid='map'
         self._camera_frameid='camera'
-        self._orb_pose_subscriber = rospy.Subscriber('/orb_slam2_mono/pose',geometry_msgs.msg.TransformStamped,self._orb_pose_grabber)
+        # self._orb_pose_sub = rospy.Subscriber('/orb_slam2_mono/pose',geometry_msgs.msg.TransformStamped,self._orb_pose_grabber)
+        self._orb_pcd_sub = rospy.Subscriber('/orb_slam2_mono/map_points',sensor_msgs.msg.PointCloud2,self._lidar_matching)
         self._track()
     
-    
+    def _lidar_matching(self,pcd_msg):
+        rospy.loginfo("Got scan, performing icp")
+        if(self._orb_pose is not None):
+            rospy.loginfo("Got scan, performing icp")
+
+        #Dummy pcd as same pcd
+        pc = ros_numpy.numpify(pcd_msg)
+        points=np.zeros((pc.shape[0],3))
+        
+        points[:,0]=pc['x']
+        points[:,1]=pc['y']
+        points[:,2]=pc['z']
+        
+        pcd = o3d.geometry.PointCloud()
+        pcd.points =o3d.utility.Vector3dVector(points)
+        
+        source=pcd;target=pcd
+        
+        reg_p2p = o3d.registration.registration_icp(
+        source, target, 0.02, np.eye(4),
+        o3d.registration.TransformationEstimationPointToPoint())
+        print(reg_p2p)
+        print("Transformation is:")
+        print(reg_p2p.transformation)
+        self._lidar_pose=reg_p2p.transformation
+        
+
+
+
     def _orb_pose_grabber(self,msg):
         
         br = tf2_ros.TransformBroadcaster()
